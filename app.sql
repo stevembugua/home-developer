42877c525e70cb65ddbd8d0ad3aed2ca077eedc4
mbugua <mbuguafaimba@gmail.com>
2022-11-01 13:12:43 +0300

    a home developer page

---
+++ app.sql
@@ -0,0 +1,7 @@
+select *from customers;
+insert into customers('firstname','secondname')
+values('steve','mbugua')
+delete from customers;
+where customer id=1
+update customers
+set contactname='mary'
